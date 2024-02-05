85b4deaf584b91a44507c6d1b9afaf43ac81cbdb
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-02-04 22:05:53 -0300

    Estilização reagindo ao useSharedValue

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { Pressable, PressableProps, Text } from 'react-native';
 
 import Animated, {
   useAnimatedStyle,
@@ -14,7 +14,7 @@ const TYPE_COLORS = {
   MEDIUM: THEME.COLORS.WARNING_LIGHT,
 }
 
-type Props = TouchableOpacityProps & {
+type Props = PressableProps & {
   title: string;
   isChecked?: boolean;
   type?: keyof typeof TYPE_COLORS;
@@ -32,8 +32,16 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
     }
   });
 
+  const onPressIn = () => {
+    scale.value = 1.2;
+  };
+
+  const onPressOut = () => {
+    scale.value = 1;
+  };
+
   return (
-    <TouchableOpacity {...rest}>
+    <Pressable onPressIn={onPressIn} onPressOut={onPressOut}{...rest}>
       <Animated.View style={
         [
           styles.container,
@@ -49,6 +57,6 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
           {title}
         </Text>
       </Animated.View>
-    </TouchableOpacity>
+    </Pressable>
   );
 }
